791aa497a5908f25b1fff4b036fad0a34b2ced09
Matt Lewis <matt@mplewis.com>
2025-08-11 13:04:17 -0600

    refactor: var -> let/const

--- common.js
+++ common.js
@@ -12,11 +12,11 @@
    * @returns {string} The value of the cookie, or empty string if not found
    */
   function getCookie(cname) {
-    var name = `${cname}=`;
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
       while (c.charAt(0) === " ") {
         c = c.substring(1);
       }
@@ -33,14 +33,14 @@
    * @returns {void}
    */
   function changeBorder() {
-    var optionInput = document.querySelector("#cardBorderChanger");
-    var curVal = 0;
+    const optionInput = document.querySelector("#cardBorderChanger");
+    let curVal = 0;
     if (optionInput.checked) {
       curVal = 11;
     }
-    var CookieDate = new Date();
-    CookieDate.setFullYear(CookieDate.getFullYear() + 1);
-    document.cookie = `cardbordersize=${curVal}; expires=${CookieDate.toUTCString()};`;
+    const cookieDate = new Date();
+    cookieDate.setFullYear(cookieDate.getFullYear() + 1);
+    document.cookie = `cardbordersize=${curVal}; expires=${cookieDate.toUTCString()};`;
     setBlackBorder(curVal);
   }
 
@@ -51,7 +51,7 @@
    * @returns {number} The padding size in pixels, or 0 if width doesn't match predefined sizes
    */
   function getNewSize(width) {
-    var newSize = 0;
+    let newSize = 0;
     switch (width) {
       case 75:
         newSize = 4;
@@ -91,10 +91,10 @@
    * @returns {void}
    */
   function setBlackBorder(bSize) {
-    var elems = document.querySelectorAll("img");
-    for (var i = 0; i < elems.length; i++) {
-      var elem = elems[i];
-      var newSize = getNewSize(elem.offsetWidth);
+    const elems = document.querySelectorAll("img");
+    for (let i = 0; i < elems.length; i++) {
+      const elem = elems[i];
+      let newSize = getNewSize(elem.offsetWidth);
       if (newSize > 0) {
         if (bSize === 0) {
           newSize = 0;
@@ -116,10 +116,10 @@
    * @returns {void}
    */
   function initBlackBorder(e) {
-    var elem = e.target.parentElement.parentElement.querySelector("img");
-    var curVal = getCookie("cardbordersize");
+    const elem = e.target.parentElement.parentElement.querySelector("img");
+    const curVal = getCookie("cardbordersize");
     if (curVal > 0) {
-      var newSize = getNewSize(elem.offsetWidth);
+      let newSize = getNewSize(elem.offsetWidth);
       if (newSize > 0) {
         if (elem.parentElement.className !== "cardborderchanger") {
           elem.outerHTML = `<span class="cardborderchanger" style="display:inline-block; padding:${newSize}px; border-radius:${newSize - 1}px; background:black;">${elem.outerHTML}</span>`;
@@ -151,8 +151,8 @@
     optionSetFunc
   ) {
     if (!document.querySelector(`#${optionId}`)) {
-      var curVal = getCookie(optionCookie);
-      var checked = "";
+      let curVal = getCookie(optionCookie);
+      let checked = "";
       if (curVal === "") {
         curVal = optionDefault;
       }
@@ -160,11 +160,11 @@
         checked = "checked";
         optionSetFunc(curVal);
       }
-      var pNavigationUl = document.querySelector("#p-navigation ul");
-      var optionLi = document.createElement("li");
+      const pNavigationUl = document.querySelector("#p-navigation ul");
+      const optionLi = document.createElement("li");
       optionLi.innerHTML = `<label for="${optionId}" style="cursor:pointer; user-select:none">${optionText}&nbsp;</label><input style="height:8px" type="checkbox" id="${optionId}" ${checked}>`;
       pNavigationUl.insertBefore(optionLi, null);
-      var optionInput = document.querySelector(`#${optionId}`);
+      const optionInput = document.querySelector(`#${optionId}`);
       optionInput.addEventListener("change", optionFunc);
     }
   }
@@ -177,7 +177,7 @@
    * @returns {void}
    */
   function fixCardPopup(e) {
-    var elem;
+    let elem;
     if (e.target) {
       elem = e.target.parentElement.nextElementSibling;
     } else {
@@ -197,14 +197,14 @@
    * @returns {void}
    */
   function fixCardPopups() {
-    var elems = document.querySelectorAll(".card-popup > a");
-    for (var i = 0; i < elems.length; i++) {
+    let elems = document.querySelectorAll(".card-popup > a");
+    for (let i = 0; i < elems.length; i++) {
       elems[i].title = "";
       elems[i].addEventListener("mouseover", fixCardPopup);
       elems[i].addEventListener("mouseover", initBlackBorder);
     }
     elems = document.querySelectorAll(".card-popup > span > img");
-    for (i = 0; i < elems.length; i++) {
+    for (let i = 0; i < elems.length; i++) {
       fixCardPopup(elems[i].parentElement);
     }
   }
@@ -215,15 +215,15 @@
    * @returns {void}
    */
   function toggleSidebarExpansions() {
-    var optionInput = document.querySelector("#showExpansionsChanger");
-    var curVal = 0;
+    const optionInput = document.querySelector("#showExpansionsChanger");
+    let curVal = 0;
     if (optionInput.checked === true) {
       curVal = 1;
     }
     setSidebarExpansions(curVal);
-    var CookieDate = new Date();
-    CookieDate.setFullYear(CookieDate.getFullYear() + 1);
-    document.cookie = `showexpansions=${curVal}; expires=${CookieDate.toUTCString()};`;
+    const cookieDate = new Date();
+    cookieDate.setFullYear(cookieDate.getFullYear() + 1);
+    document.cookie = `showexpansions=${curVal}; expires=${cookieDate.toUTCString()};`;
   }
 
   /**
@@ -239,12 +239,12 @@
         curVal = 1;
       }
     }
-    var visibility = "none";
+    let visibility = "none";
     if (curVal === 1) {
       visibility = "block";
     }
-    var elems = document.querySelectorAll(".showExpansionItem");
-    for (var i = 0; i < elems.length; i++) {
+    const elems = document.querySelectorAll(".showExpansionItem");
+    for (let i = 0; i < elems.length; i++) {
       elems[i].style.display = visibility;
     }
   }
@@ -257,10 +257,10 @@
    * @returns {void}
    */
   function addExpansionLink(link, title) {
-    var pNavigationUl = document.querySelector("#p-navigation ul");
-    var optionLi = document.createElement("li");
+    const pNavigationUl = document.querySelector("#p-navigation ul");
+    const optionLi = document.createElement("li");
     optionLi.classList.add("showExpansionItem");
-    var urlBase = mw.config.values.wgArticlePath.replace("$1", "");
+    const urlBase = mw.config.values.wgArticlePath.replace("$1", "");
     optionLi.innerHTML = `<a href="${urlBase}${link}">${title}</a>`;
     pNavigationUl.insertBefore(optionLi, null);
   }
@@ -272,9 +272,9 @@
    * @returns {void}
    */
   function addExpansionSidebarLinks() {
-    var pNavigationUl = document.querySelector("#p-navigation ul");
+    const pNavigationUl = document.querySelector("#p-navigation ul");
     if (pNavigationUl && !document.querySelector("#expansionSidebarLinks")) {
-      var expansionSidebarLinks = document.createElement("span");
+      const expansionSidebarLinks = document.createElement("span");
       expansionSidebarLinks.id = "expansionSidebarLinks";
       pNavigationUl.insertBefore(expansionSidebarLinks, null);
       addExpansionLink("Dominion (Base Set)", "Dominion");
@@ -307,20 +307,20 @@
    * @returns {void}
    */
   function sortSortables(startsort, sortby, sortid) {
-    var cardsByName = [];
-    var cardsByCostName = [];
-    var hasNonZeroCost = false;
-    var elems = startsort.querySelectorAll(".cardcost");
-    for (var i = 0; i < elems.length; i++) {
-      var cardname = elems[i].querySelector("a").title;
+    const cardsByName = [];
+    const cardsByCostName = [];
+    let hasNonZeroCost = false;
+    const elems = startsort.querySelectorAll(".cardcost");
+    for (let i = 0; i < elems.length; i++) {
+      const cardname = elems[i].querySelector("a").title;
       cardsByName.push([cardname, elems[i]]);
-      for (var j = 0; j < elems[i].classList.length; j++) {
-        var costplus = elems[i].classList[j];
-        var re = /^cost/i;
-        var found = costplus.match(re);
+      for (let j = 0; j < elems[i].classList.length; j++) {
+        const costplus = elems[i].classList[j];
+        const re = /^cost/i;
+        const found = costplus.match(re);
         if (found) {
-          var extrachar = "";
-          var lastchar = costplus.charAt(costplus.length - 1).toLowerCase();
+          let extrachar = "";
+          const lastchar = costplus.charAt(costplus.length - 1).toLowerCase();
           if (
             lastchar !== "*" &&
             lastchar !== "+" &&
@@ -329,7 +329,7 @@
           ) {
             extrachar = "!";
           }
-          var costpluscardname = `${costplus}${extrachar}${cardname}`;
+          const costpluscardname = `${costplus}${extrachar}${cardname}`;
           cardsByCostName.push([costpluscardname, elems[i]]);
           if (
             costplus !== "cost$00" &&
@@ -342,11 +342,11 @@
         }
       }
     }
-    var sortlist = [];
+    let sortlist = [];
     if (sortby === "sortbyname") {
       cardsByName.sort(function (a, b) {
-        var a0 = a[0].toLowerCase();
-        var b0 = b[0].toLowerCase();
+        const a0 = a[0].toLowerCase();
+        const b0 = b[0].toLowerCase();
         if (a0 < b0) {
           return -1;
         }
@@ -358,8 +358,8 @@
       sortlist = cardsByName;
     } else {
       cardsByCostName.sort(function (a, b) {
-        var a0 = a[0].toLowerCase();
-        var b0 = b[0].toLowerCase();
+        const a0 = a[0].toLowerCase();
+        const b0 = b[0].toLowerCase();
         if (a0 < b0) {
           return -1;
         }
@@ -370,23 +370,23 @@
       });
       sortlist = cardsByCostName;
     }
-    for (i = 0; i < sortlist.length; i++) {
+    for (let i = 0; i < sortlist.length; i++) {
       startsort.insertBefore(sortlist[i][1], null);
     }
-    elems = document.querySelectorAll(`.switchsort.sortbyname.${sortid}`);
-    for (i = 0; i < elems.length; i++) {
+    let switchElems = document.querySelectorAll(`.switchsort.sortbyname.${sortid}`);
+    for (let i = 0; i < switchElems.length; i++) {
       if (sortby === "sortbyname" || !hasNonZeroCost) {
-        elems[i].style.display = "none";
+        switchElems[i].style.display = "none";
       } else {
-        elems[i].style.display = "";
+        switchElems[i].style.display = "";
       }
     }
-    elems = document.querySelectorAll(`.switchsort.sortbycost.${sortid}`);
-    for (i = 0; i < elems.length; i++) {
+    switchElems = document.querySelectorAll(`.switchsort.sortbycost.${sortid}`);
+    for (let i = 0; i < switchElems.length; i++) {
       if (sortby === "sortbycost" || !hasNonZeroCost) {
-        elems[i].style.display = "none";
+        switchElems[i].style.display = "none";
       } else {
-        elems[i].style.display = "";
+        switchElems[i].style.display = "";
       }
     }
   }
@@ -398,22 +398,22 @@
    * @returns {void}
    */
   function startSort(e) {
-    var sortby = "";
-    var sortid = "";
-    for (var i = 0; i < e.target.classList.length; i++) {
-      var re = /^sortby/i;
-      var found = e.target.classList[i].match(re);
+    let sortby = "";
+    let sortid = "";
+    for (let i = 0; i < e.target.classList.length; i++) {
+      const re = /^sortby/i;
+      const found = e.target.classList[i].match(re);
       if (found) {
         sortby = e.target.classList[i];
       }
-      var re2 = /^sortid/i;
-      var found2 = e.target.classList[i].match(re2);
+      const re2 = /^sortid/i;
+      const found2 = e.target.classList[i].match(re2);
       if (found2) {
         sortid = e.target.classList[i];
       }
     }
-    var elems = document.querySelectorAll(`.startsort.${sortid}`);
-    for (i = 0; i < elems.length; i++) {
+    const elems = document.querySelectorAll(`.startsort.${sortid}`);
+    for (let i = 0; i < elems.length; i++) {
       sortSortables(elems[i], sortby, sortid);
     }
   }
@@ -424,12 +424,12 @@
    * @returns {void}
    */
   function initSorting() {
-    var elems = document.querySelectorAll(".switchsort");
-    for (var i = 0; i < elems.length; i++) {
+    let elems = document.querySelectorAll(".switchsort");
+    for (let i = 0; i < elems.length; i++) {
       elems[i].addEventListener("click", startSort);
     }
     elems = document.querySelectorAll(".switchsort.sortbyname");
-    for (i = 0; i < elems.length; i++) {
+    for (let i = 0; i < elems.length; i++) {
       elems[i].click();
     }
   }
@@ -448,13 +448,13 @@
       }
     }
     if (curVal === 1) {
-      var elems = document.querySelectorAll(".switchsort.sortbycost");
-      for (var i = 0; i < elems.length; i++) {
+      const elems = document.querySelectorAll(".switchsort.sortbycost");
+      for (let i = 0; i < elems.length; i++) {
         elems[i].click();
       }
     } else {
-      var elems = document.querySelectorAll(".switchsort.sortbyname");
-      for (var i = 0; i < elems.length; i++) {
+      const elems = document.querySelectorAll(".switchsort.sortbyname");
+      for (let i = 0; i < elems.length; i++) {
         elems[i].click();
       }
     }
@@ -466,14 +466,14 @@
    * @returns {void}
    */
   function changeCardSortBy() {
-    var optionInput = document.querySelector("#cardGallerySorter");
-    var curVal = 0;
+    const optionInput = document.querySelector("#cardGallerySorter");
+    let curVal = 0;
     if (optionInput.checked) {
       curVal = 1;
     }
-    var CookieDate = new Date();
-    CookieDate.setFullYear(CookieDate.getFullYear() + 1);
-    document.cookie = `cardsortby=${curVal}; expires=${CookieDate.toUTCString()};`;
+    const cookieDate = new Date();
+    cookieDate.setFullYear(cookieDate.getFullYear() + 1);
+    document.cookie = `cardsortby=${curVal}; expires=${cookieDate.toUTCString()};`;
     setCardSortBy(curVal);
   }
 
@@ -483,15 +483,15 @@
    * @returns {void}
    */
   function toggleNavboxImages() {
-    var optionInput = document.querySelector("#hoverInsideCollapsibles");
-    var curVal = 0;
+    const optionInput = document.querySelector("#hoverInsideCollapsibles");
+    let curVal = 0;
     if (optionInput.checked === true) {
       curVal = 1;
     }
     setNavboxImages(curVal);
-    var CookieDate = new Date();
-    CookieDate.setFullYear(CookieDate.getFullYear() + 1);
-    document.cookie = `hoverinsidecollapsibles=${curVal}; expires=${CookieDate.toUTCString()};`;
+    const cookieDate = new Date();
+    cookieDate.setFullYear(cookieDate.getFullYear() + 1);
+    document.cookie = `hoverinsidecollapsibles=${curVal}; expires=${cookieDate.toUTCString()};`;
   }
 
   /**
@@ -514,7 +514,7 @@
     }
   }
 
-  var clickedThings = false;
+  let clickedThings = false;
   /**
    * Auto-expands collapsible elements on the Legacy All Cards Navbox page.
    * Uses recursive setTimeout to keep trying until the collapsible element is found and clicked.
@@ -522,7 +522,7 @@
    */
   function clickThings() {
     if (window.location.href.search("Legacy_All_Cards_Navbox") !== -1) {
-      var thingToClick = document.querySelector(".mw-collapsible-text");
+      const thingToClick = document.querySelector(".mw-collapsible-text");
       if (thingToClick) {
         thingToClick.click();
         clickedThings = true;
