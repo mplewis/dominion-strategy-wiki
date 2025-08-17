342f612c411f34b6be3d94736d5aeec0165cbabd
Matt Lewis <matt@mplewis.com>
2025-08-17 17:40:44 -0600

    HACK: debug init sidebar

--- src/wiki/core/initialization.ts
+++ src/wiki/core/initialization.ts
@@ -12,12 +12,30 @@ import { initSiteOptions, triggerAllSiteOptions } from "./options";
  * @returns Promise that resolves when initialization is complete
  */
 export async function initCommon(): Promise<void> {
+	console.debug(`[initCommon] Starting common initialization`);
+	console.debug(`[initCommon] Initializing card popups`);
 	initCardPopups();
+	console.debug(`[initCommon] Card popups initialized`);
 
+	console.debug(`[initCommon] Initializing site options`);
 	await initSiteOptions();
-	addExpansionSidebarLinks();
-	await triggerAllSiteOptions();
+	console.debug(`[initCommon] Site options initialized`);
 
+	console.debug(`[initCommon] Adding expansion sidebar links`);
+	addExpansionSidebarLinks();
+	console.debug(`[initCommon] Expansion sidebar links added`);
+
+	console.debug(`[initCommon] Triggering all site options`);
+	await triggerAllSiteOptions();
+	console.debug(`[initCommon] All site options triggered`);
+
+	console.debug(`[initCommon] Initializing sorting`);
 	initSorting();
+	console.debug(`[initCommon] Sorting initialized`);
+
+	console.debug(`[initCommon] Expanding all cards elements`);
 	expandAllCardsElements();
+	console.debug(`[initCommon] All cards elements expanded`);
+
+	console.debug(`[initCommon] Common initialization completed`);
 }
